d90431376f5a77c140c9b3e6cf4b046258c4a3df
Dhanush R Shetty <dhanushetty3@gmail.com>
2026-03-20 18:22:27 +0000

    the logos have not been correctly implemented

--- src/components/icons/SmsIcon.tsx
+++ src/components/icons/SmsIcon.tsx
@@ -10,7 +10,7 @@ export function SmsIcon(props: SVGProps<SVGSVGElement>) {
       fill="currentColor"
       {...props}
     >
-      <path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"></path>
+      <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM9 11H7V9h2v2zm4 0h-2V9h2v2zm4 0h-2V9h2v2z"></path>
     </svg>
   );
 }
