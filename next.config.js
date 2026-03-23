e7d7d22bd577a9ef88c318f0c9d5f5f775140c13
Dhanush R Shetty <36324139+Dhanushetty3@users.noreply.github.com>
2026-03-23 13:12:05 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/YOUR-REPO-NAME",
+  assetPrefix: "/YOUR-REPO-NAME/",
+};
+
+module.exports = nextConfig;
