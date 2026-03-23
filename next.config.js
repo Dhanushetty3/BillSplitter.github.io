f7d79f9596a0bfb83b9b308ebdda7a0d77181dcb
Dhanush R Shetty <dhanushetty3@gmail.com>
2026-03-23 08:21:44 +0000

    not able to create git pages yet

--- next.config.js
+++ next.config.js
@@ -1,10 +1,37 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/BillSplitter",
+  assetPrefix: "/BillSplitter/",
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
-  basePath: "/YOUR-REPO-NAME",
-  assetPrefix: "/YOUR-REPO-NAME/",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
